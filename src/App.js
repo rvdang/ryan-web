6e0adcbb7da2194cdf429eb1cb0c59f9838a8419
rvdang <rvdang98@gmail.com>
2019-09-22 16:00:04 -0700

    temporarily remove projects

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={AboutMe} />
             <Route exact path="/resume" component={Resume} />
-            <Route exact path="/projects" component={Projects} />
+            {/* <Route exact path="/projects" component={Projects} /> */}
             <Route exact path="/contact" component={Contact} />
           </Switch>
         </Router>
