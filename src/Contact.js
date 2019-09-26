2cf35a6a872a810c3ddd560c090325627d81cc0d
rvdang <rvdang@uci.edu>
2019-09-26 02:16:12 -0700

    remove justify content for safari compatibility

--- src/Contact.js
+++ src/Contact.js
@@ -2,20 +2,22 @@ import React from "react";
 import { LinkedIn, GitHub, Mail } from "./icons/iconSVG";
 export const Contact = props => (
   <div id="contact" className="shortFadeIn page horizontal maxswitch">
-    <span className="contact" id="mail">
-      <a className="contact" href="mailto:rvdang@uci.edu">
-        <Mail className="contact" />
-      </a>
-    </span>
-    <span className="contact">
-      <a className="contact" href={"https://github.com/rvdang"}>
-        <GitHub className="contact" />
-      </a>
-    </span>
-    <span className="contact">
-      <a className="contact" href={"https://linkedin.com/in/rvdang"}>
-        <LinkedIn className="contact" />
-      </a>
-    </span>
+    <div style={{margin: "auto", display: "flex", flexDirection: "row-reverse", textAlign: "center"}}>
+      <span className="contact" id="mail">
+        <a className="contact" href="mailto:rvdang@uci.edu">
+          <Mail className="contact" />
+        </a>
+      </span>
+      <span className="contact">
+        <a className="contact" href={"https://github.com/rvdang"}>
+          <GitHub className="contact" />
+        </a>
+      </span>
+      <span className="contact">
+        <a className="contact" href={"https://linkedin.com/in/rvdang"}>
+          <LinkedIn className="contact" />
+        </a>
+      </span>
+    </div>
   </div>
 );
