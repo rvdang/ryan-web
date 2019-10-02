f9eb104f7813333ef69c82718d979727ec7c2648
rvdang <rvdang@uci.edu>
2019-10-01 23:32:06 -0700

    hashrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { Projects } from "./project";
 import { LoadingScreen, Navbar, Badge, Footer, Mountain } from "./loading";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { AboutMe } from "./AboutMe";
 import { Contact } from "./Contact";
 import { Resume } from "./pages/resume";
@@ -13,13 +13,13 @@ function App() {
     <LoadingScreen>
       <meta name="viewport" content="width=1024" />
       <div className="max fadeIn">
-        <Router>
+        <Router basename="/">
           <Navbar />
           <Switch>
             <Route exact path="/" component={AboutMe} />
-            <Route path="/resume" component={Resume} />
+            <Route exact path="/resume" component={Resume} />
             {/* <Route exact path="/projects" component={Projects} /> */}
-            <Route path="/contact" component={Contact} />
+            <Route exact path="/contact" component={Contact} />
           </Switch>
         </Router>
         {/* <Mountain/> */}
